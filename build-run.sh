3b0bbf46e834393b121d590421c4a9e694fe94bd
hebertluiz <hebert.luiz.hb@gmail.com>
2022-04-30 20:25:05 -0300

    changed functions location

--- build-run.sh
+++ build-run.sh
@@ -9,45 +9,9 @@ BRIEF='Script Packer:
 '
 ## Global variables
 programname=$0
-VERBOSE=0
 
+source ./functions/log_console.sh
 
-## Error types
-declare -A errors
-errors[0]="Generic Error   "
-errors[1]="File Error      "
-errors[2]="Running as root "
-errors[3]="Invalid Argument"
-
-
-
-function error () {
-    if [ "$1" -ne 0 ];
-    then 
-        printf "%s -[%s] %s\n" "$(date +%D\ %T)" "${errors[$1]}" "$2" 1>&2
-        exit "$1"
-    else 
-        printf "%s -[%s] %s\n" "$(date +%D\ %T)" "${errors[$1]}" "$2" 1>&2
-    fi
-}
-
-
-declare -A levels
-levels[0]="INFO"
-levels[1]="NOTICE"
-levels[2]="ALERT"
-levels[3]="TRACE"
-levels[4]="ERROR"
-
-
-function verbose () {
-    if [ "$1" -ge "1" ]
-    then 
-        printf "%s -[%s] %s\n" "$(date +%D\ %T)" "${levels[$1]}" "$2" 1>&2
-    else 
-        printf "%s\n" "$2" 1>&2
-    fi
-}
 
 function usage () {
     [ "$1" = "error" ] || echo "$HEADER"
