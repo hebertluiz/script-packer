ca97633556567d705e3bdc6a4aad14f6389d8c6b
hebertluiz <hebert.luiz.hb@gmail.com>
2022-04-30 20:02:34 -0300

    Isolating console output and errors

---
+++ functions/log_console.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+
+## Error types
+declare -A errors
+errors[0]="Generic Error   "
+errors[1]="File Error      "
+errors[2]="Running as root "
+errors[3]="Invalid Argument"
+
+
+
+function error () {
+    if [ "$1" -ne 0 ];
+    then 
+        printf "%s -[%s] %s\n" "$(date +%D\ %T)" "${errors[$1]}" "$2" 1>&2
+        exit "$1"
+    else 
+        printf "%s -[%s] %s\n" "$(date +%D\ %T)" "${errors[$1]}" "$2" 1>&2
+    fi
+}
+
+
+declare -A levels
+levels[0]="INFO"
+levels[1]="NOTICE"
+levels[2]="ALERT"
+levels[3]="TRACE"
+levels[4]="ERROR"
+
+
+function verbose () {
+    if [ "$1" -ge "1" ]
+    then 
+        printf "%s -[%s] %s\n" "$(date +%D\ %T)" "${levels[$1]}" "$2" 1>&2
+    else 
+        printf "%s\n" "$2" 1>&2
+    fi
+}
+
