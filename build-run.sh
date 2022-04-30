7c87b2b415b2c1ada08029ae9553e5e24d81a7e1
hebertluiz <hebert.luiz.hb@gmail.com>
2022-04-30 19:56:53 -0300

    Handle errors and fixes
    Improved debug with verbose and error
    May be migrated to a dedicated git repo

--- build-run.sh
+++ build-run.sh
@@ -1,81 +1,169 @@
 #!/bin/bash
 
-HEADER='
-Author: Hebert L Silva | https://github.com/hebertluiz
+## Text 
+HEADER='Author: Hebert L Silva | https://github.com/hebertluiz
 License: MIT
-
 '
-BRIEF='
-Simple script/environment to build a self extracting shellscript installer
-
+BRIEF='Script Packer:
+    Simple script/environment to build a self extracting shellscript installer
 '
-
+## Global variables
 programname=$0
-VERBOSE=false
+VERBOSE=0
 
-function verbose () {
-    $VERBOSE && printf "%s -| VERBOSE: %s\n" "$(date +%D\ %T)" "$*" 1>&2
-}
+
+## Error types
+declare -A errors
+errors[0]="Generic Error   "
+errors[1]="File Error      "
+errors[2]="Running as root "
+errors[3]="Invalid Argument"
+
+
 
 function error () {
-    printf "%s -| ERROR: %s\n" "$(date +%D\ %T)" "$*" 1>&2
+    if [ "$1" -ne 0 ];
+    then 
+        printf "%s -[%s] %s\n" "$(date +%D\ %T)" "${errors[$1]}" "$2" 1>&2
+        exit "$1"
+    else 
+        printf "%s -[%s] %s\n" "$(date +%D\ %T)" "${errors[$1]}" "$2" 1>&2
+    fi
+}
+
+
+declare -A levels
+levels[0]="INFO"
+levels[1]="NOTICE"
+levels[2]="ALERT"
+levels[3]="TRACE"
+levels[4]="ERROR"
+
+
+function verbose () {
+    if [ "$1" -ge "1" ]
+    then 
+        printf "%s -[%s] %s\n" "$(date +%D\ %T)" "${levels[$1]}" "$2" 1>&2
+    else 
+        printf "%s\n" "$2" 1>&2
+    fi
 }
 
 function usage () {
-    echo "$HEADER"
-    echo "Usage: $programname [-vD|-h] <-i outfile.tar.gz> <-d </dir/to/pack/>"  
-    echo "  -i <FILEPATH>"  
-    echo "  -d <FILEPATH>   Source directory for the package"  
-    echo "  -v              Show verbose output"
-    echo "  -D              Show debug output  "
+    [ "$1" = "error" ] || echo "$HEADER"
+    cat << USAGE_MSG
+Usage: $programname [-vDc|-h] [-p PACKAGE_NAME ] 
+    [-o run.sh ] <-d </dir/to/pack/>|-f <TARFILE_PATH>>  
+
+Comand-line Options:
+    -c                      Compress outfile with gzip
+    -d <DIR_PATH>           Source directory for the package  
+    -f <TARFILE_PATH>       Tar file to embed into script outfile
+    -h                      Show this help message
+    -o <FILE_PATH>          Outfile, optional. Will be set as <PACKAGE_NAME->run.sh  
+    -p <PACKAGE_NAME>       Name of output package
+    -v                      Show verbose output
+    -D                      Show debug output (implies verbose on)
+USAGE_MSG
+
+
 }
 
 #### Parsing OPTS
-while getopts ":i:d:vhD" opt; do
+while getopts "f:d:o:p:v:hD" opt; do
     case ${opt} in 
-        i) #opt Install outfile
-            outfile_package="$OPTARG"
+        p) #opt packagename
+            package_name="${OPTARG:-package}"
+        ;;
+        o) #opt Install outfile
+
+            # FIXME Better outfile path sanitizing 
+            outfile_name="$(basename "$OPTARG")"
+            unsafe_path="$(dirname "$OPTARG")"
+            outfile_path="${unsafe_path:-.}"
+            unset unsafe_path
+
         ;;
         d) # dir to pack
             dir_to_pack="$OPTARG"
         ;; 
-        v) 
-            VERBOSE=true
+        f) # tar file to include in script
+            tar_to_pack="$OPTARG"
         ;;
-        D) 
+        v) # set verbose output
+            if [ "$OPTARG" = ':' ];
+            then 
+                VERBOSE++ 
+            fi
+        ;;
+        D) # set debug and verbose 
             VERBOSE=true
             set -vx
         ;;
-        h) 
+        h) # show help and info
             echo "$BRIEF" 1>&2
-            usage 1>&2
+            usage "" 1>&2
+            exit 0
         ;;
-        \?) 
-            error "Invalid Option: -$OPTARG" 
-            usage 1>&2
+        \?) # Generic case for invalid options 
+            usage error 1>&2
+            error 3 "Invalid Option: -$OPTARG" 
         ;;
     esac
 done
+shift $((OPTIND -1))
 
-verbose "Checking Options:"
-verbose "Outfile: $outfile_package"
-verbose "Dir To Pack: $dir_to_pack"
-
-## check if we can write to output file 
-if [ -z "$outfile_package" ];
-then 
-    error Invalid outfile
-    usage
-    exit 1
-else
-    : # TODO Check if file exists to prevent overwrite.
+if [ "$(id -u)" -eq '0' ];
+then
+    error 2 "Not supported"
 fi
 
-if [ ! -d "$dir_to_pack" ];
+## Check output file
+# Preventing actin on / (root)
+[ -z "$outfile_path" ] && outfile_path='.'
+
+if ! err="$(touch "$outfile_path/$outfile_name" 2>&1)";
 then 
-    error Source dir does not exist
-    usage
-    exit 1
+    error 0 "Cant create Outfile: $outfile_name"
+    error 1 "Reason: $err"
+
+elif [ -s "$outfile_path/${outfile_name:-run.sh}" ]
+then 
+    # TODO Check if file exists to prevent overwrite.
+    error 1 "File $outfile_name is not empty"
+
+fi
+
+outfile="$outfile_path/${outfile_name:-run.sh}"
+
+
+## Check input files
+if [ -z "$tar_to_pack" ]; 
+then 
+    PACK_DIR=true 
+    
+    if [ ! -d "$dir_to_pack" ];
+    then 
+        error 1 "Source dir does not exist"
+    fi
+
+    if [ ! -r "$dir_to_pack" ];
+    then 
+        error 1 "Fail to read files in source directory" 
+    fi
+else
+    
+    PACK_DIR=false
+    if [ ! -r "$tar_to_pack" ]
+    then
+        error 1 "Cant read $tar_to_pack"
+    fi
+
+    if ! tar tf "$tar_to_pack" 
+    then
+        error 1 "Invalid Tar File $tar_to_pack"
+    fi 
+    
 fi
 
 
@@ -87,7 +175,17 @@ fi
 # shellcheck disable=SC2034  # Unused variables left temporarily
 template_bootstrap_script="template-run.sh"; 
 # shellcheck disable=SC2034
-packed_file=install.tar.gz
+packed_file=package.tar.gz
+
+echo "Outfile: $outfile"
+echo "Package Name: $package_name"
+
+if $PACK_DIR 
+then 
+    echo "Dir To Pack: $dir_to_pack"
+else
+    echo "Tar To Pack: $tar_to_pack"
+fi
 
 
 
